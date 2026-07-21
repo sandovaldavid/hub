363c311d41e61945d43ce57a0d7ba823f142d76c
David Sandoval <dev@sandovaldavid.com>
2026-07-21 10:00:19 -0500

    refactor(architecture): import profile type directly

--- src/widgets/hero-section/model/types.ts
+++ src/widgets/hero-section/model/types.ts
@@ -1,4 +1,4 @@
-import type { Profile } from '../../../entities/profile';
+import type { Profile } from '../../../entities/profile/model/types';
 
 export interface HeroMetadataItem {
 	label: string;
